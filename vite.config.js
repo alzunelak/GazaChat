9aa9b728a97672747fa83a177091b6ec47afa01a
alzunelak <122785535+alzunelak@users.noreply.github.com>
2025-08-12 18:25:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ChatApp/', // Ensure this matches your repository name
+  base: '/ChatApp/', 
 });
