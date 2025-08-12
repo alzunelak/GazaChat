f2fd53411382a934da0edbca2974c8f3ea0c37ae
alzunelak <122785535+alzunelak@users.noreply.github.com>
2025-08-12 18:25:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/https://alzunelak.github.io/ChatApp/",  // Replace with your repo name
-  plugins: [react()]
+  plugins: [react()],
+  base: '/ChatApp/', // Ensure this matches your repository name
 });
