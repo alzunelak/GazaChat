ed4c330af2fcbbcbc1877929a691d77e66cc2916
alzunelak <122785535+alzunelak@users.noreply.github.com>
2025-08-14 07:27:37 +0300

    Create navgraph.kt

---
+++ navgraph.kt
@@ -0,0 +1,27 @@
+package com.example.chatbt.nav
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.chatbt.ui.screens.*
+
+@Composable
+fun NavGraph() {
+    val nav: NavHostController = rememberNavController()
+    NavHost(navController = nav, startDestination = "splash") {
+        composable("splash") { SplashScreen { nav.navigate("name") { popUpTo("splash"){inclusive=true} } } }
+        composable("name")   { NameScreen(onDone = { nav.navigate("home"){ popUpTo("name"){inclusive=true} } }) }
+        composable("home")   { HomeScreen(
+            onScan = { nav.navigate("scan") },
+            onSearch = { nav.navigate("search") },
+            onMyQr = { nav.navigate("myqr") },
+            onChat = { nav.navigate("chat") }
+        ) }
+        composable("scan")   { ScanScreen(onFriendReady = { nav.navigate("home") }) }
+        composable("search") { SearchScreen(onFriendReady = { nav.navigate("home") }) }
+        composable("chat")   { ChatScreen() }
+        composable("myqr")   { MyQrScreen() }
+    }
+}
