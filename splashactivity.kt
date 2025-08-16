4ebc95076058a4d02a12ad2c116fbdcfeb877245
alzunelak <122785535+alzunelak@users.noreply.github.com>
2025-08-16 09:18:26 +0300

    Update splashactivity.kt

--- splashactivity.kt
+++ splashactivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
 import android.os.Looper
 
-class SplashActivity : AppCompatActivity() {
+class splashactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
